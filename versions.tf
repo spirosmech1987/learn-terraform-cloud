465dfd382a74af4e31183b6fa90606ce63fdd078
Spyros Kapodistrias <kapodis@amazon.com>
2022-06-14 13:42:56 +0100

    Add auto.tfvars file

--- versions.tf
+++ versions.tf
@@ -1,13 +1,13 @@
 terraform {
-
+/*
   cloud {
-    organization = "hashicorp-learn"
+    organization = "terraform-tutorialssk"
 
     workspaces {
       name = "learn-terraform-cloud"
     }
   }
-
+*/
   required_providers {
     aws = {
       source  = "hashicorp/aws"
